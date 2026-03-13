41964bc46aa624a29fdf6d494b33ded71a0ce33a
dkshreeblocktech <dkshreeblocktech@gmail.com>
2026-03-13 13:04:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,21 +7,21 @@ app.use(express.json());
 mongoose.connect("mongodb+srv://admin:admin123@cluster0.uhgapf3.mongodb.net/students");
 
 const Student = mongoose.model("details",{
-Name:String,
-FatherName:String,
-MotherName:String,
-Location:String
+  name:String,
+  fathername:String,
+  mothername:String,
+  location:String
 });
 
 app.post("/add", async (req,res)=>{
 
-const data = new Student(req.body);
-await data.save();
+  const data = new Student(req.body);
+  await data.save();
 
-res.send("Saved");
+  res.send("Saved");
 
 });
 
-app.listen(3000, ()=>{
-console.log("Server running");
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log("Server running");
 });
