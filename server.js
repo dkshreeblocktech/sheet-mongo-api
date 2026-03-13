ee48ce5fd902d77733d55a7244693d46c1e514a1
dkshreeblocktech <dkshreeblocktech@gmail.com>
2026-03-13 15:55:06 +0530

    add server.js

--- server.js
+++ server.js
@@ -4,27 +4,35 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://admin:admin123@cluster0.uhgapf3.mongodb.net/students",{
-  useNewUrlParser:true,
-  useUnifiedTopology:true
-});
+mongoose.connect(
+"mongodb+srv://admin:admin123@cluster0.uhgapf3.mongodb.net/students?retryWrites=true&w=majority"
+).then(()=>console.log("MongoDB Connected"));
 
 const Student = mongoose.model("details",{
-  name:String,
-  fathername:String,
-  mothername:String,
-  location:String
+name:String,
+fathername:String,
+mothername:String,
+location:String
 });
 
 app.post("/add", async (req,res)=>{
 
-  const data = new Student(req.body);
-  await data.save();
+try{
 
-  res.send("Saved");
+const data = new Student(req.body);
+await data.save();
+
+res.send("Saved");
+
+}catch(err){
+
+console.log(err);
+res.send("Error");
+
+}
 
 });
 
 app.listen(process.env.PORT || 3000, ()=>{
-  console.log("Server running");
+console.log("Server running");
 });
